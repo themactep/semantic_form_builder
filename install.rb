c291c7a848acc4d42450184385f6e13b70cd1159
Paul Philippov <themactep@gmail.com>
2010-03-29 06:22:59 +0700

    scripts to install / uninstall assets

--- install.rb
+++ install.rb
@@ -1,11 +1,8 @@
-# Copy the assets into RAILS_ROOT/public/
+# Copy the assets into RAILS_ROOT
 RAILS_ROOT = File.join(File.dirname(__FILE__), '../../../')
-
-FileUtils.cp_r( 
-  Dir[File.join(File.dirname(__FILE__), 'public')], 
-  File.join(RAILS_ROOT),
-  :verbose => true
-)
+FileUtils.cp_r(Dir[File.join(File.dirname(__FILE__), 'app')],    File.join(RAILS_ROOT), :verbose => true)
+FileUtils.cp_r(Dir[File.join(File.dirname(__FILE__), 'config')], File.join(RAILS_ROOT), :verbose => true)
+FileUtils.cp_r(Dir[File.join(File.dirname(__FILE__), 'public')], File.join(RAILS_ROOT), :verbose => true)
 
 # Show the README text file
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
