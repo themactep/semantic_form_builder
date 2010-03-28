629e8a82c4dbfb99759caa41df4505f0168efc1b
Paul Philippov <themactep@gmail.com>
2010-03-29 06:23:44 +0700

    load our FormBuilder by default

--- init.rb
+++ init.rb
@@ -1,1 +1,5 @@
-# Include hook code here
+require 'semantic_form_builder'
+require 'semantic_form_helper'
+require 'semantic_form_errors'
+
+ActionView::Base.default_form_builder = SemanticFormBuilder
