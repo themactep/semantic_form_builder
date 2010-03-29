473987e43d380ffaffec41b67d66a64e9dfcce45
Paul Philippov <themactep@gmail.com>
2010-03-29 17:44:00 +0700

    do not delete unexistent file

--- uninstall.rb
+++ uninstall.rb
@@ -1,6 +1,5 @@
 # Remove the assets from RAILS_ROOT
 RAILS_ROOT = File.join(File.dirname(__FILE__), '../../../')
-FileUtils.unlink File.join(RAILS_ROOT, 'app', 'stylesheets', '_form_palette.less')
 FileUtils.unlink File.join(RAILS_ROOT, 'app', 'stylesheets', 'semantic_forms.less')
 FileUtils.unlink File.join(RAILS_ROOT, 'config', 'locales', 'form_builder', 'en.yml')
 FileUtils.unlink File.join(RAILS_ROOT, 'public', 'images', '16x16_spinner.gif')
