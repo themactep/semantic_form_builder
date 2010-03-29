3d04f70e01127ebb9cc04a0d39ca41b15965be79
Paul Philippov <themactep@gmail.com>
2010-03-29 17:45:22 +0700

    format code

--- lib/semantic_form_errors.rb
+++ lib/semantic_form_errors.rb
@@ -19,30 +19,28 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   end
 end
 
-module ActionView::Helpers::ActiveRecordHelper
+module ActionView
+  module Helpers
+    module  ActiveRecordHelper
 
-  def error_message_on(object, method, *args)
-    options = args.extract_options!
-    unless args.empty?
-      ActiveSupport::Deprecation.warn(
-        'error_message_on takes an option hash instead of separate ' +
-        'prepend_text, append_text, and css_class arguments', caller
-      )
+      def error_message_on(object, method, *args)
+        options = args.extract_options!
+        unless args.empty?
+          ActiveSupport::Deprecation.warn 'error_message_on takes an option hash instead of separate prepend_text, append_text, and css_class arguments', caller
+          options[:prepend_text] = args[0] || ''
+          options[:append_text]  = args[1] || ''
+          options[:css_class]    = args[2] || 'form_error'
+        end
+        options.reverse_merge!({:prepend_text => '', :append_text => '', :css_class => 'form_error'})
 
-      options[:prepend_text] = args[0] || ''
-      options[:append_text]  = args[1] || ''
-      options[:css_class]    = args[2] || 'form_error'
-    end
-    options.reverse_merge!(:prepend_text => '', :append_text => '', :css_class => 'form_error')
+        if (obj = (object.respond_to?(:errors) ? object : instance_variable_get("@#{object}"))) &&
+          errors = obj.errors.on(method)
+          content_tag 'span', "#{options[:prepend_text]}#{ERB::Util.html_escape(errors.is_a?(Array) ? errors.first : errors)}#{options[:append_text]}", :class => options[:css_class]
+        else
+          ''
+        end
+      end
 
-    if (obj = (object.respond_to?(:errors) ? object : instance_variable_get("@#{object}"))) &&
-      (errors = obj.errors.on(method))
-      content_tag('span',
-        "#{options[:prepend_text]}#{ERB::Util.html_escape(errors.is_a?(Array) ? errors.first : errors)}#{options[:append_text]}",
-        :class => options[:css_class]
-      )
-    else
-      ''
     end
   end
 end
